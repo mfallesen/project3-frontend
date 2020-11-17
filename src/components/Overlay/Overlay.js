4cae2f8908c948e1a9fb19682d4256400268b6c1
Andrew Crow <andrew@crow.me>
2020-11-17 00:24:42 -0800

    added Overlay component

---
+++ src/components/Overlay/Overlay.js
@@ -0,0 +1,31 @@
+import './Overlay.css'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid"
+
+
+
+const useStyles = makeStyles({
+    root: {
+        paddingTop: "200px",
+    },
+
+});
+
+export default function Overlay(props) {
+    const classes = useStyles();
+
+    return (
+        <Grid container className={classes.root}>
+            <Grid item xs={12}>
+                <Typography>
+                    <h1>Memorable Microadventures</h1>
+                </Typography>
+                <Button className={props.btn}>Sign Up</Button>
+            </Grid>
+        </Grid>
+    );
+}
+
