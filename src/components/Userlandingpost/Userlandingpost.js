67032135020c348e17956e5f5ffa9abc71bceeac
Mike Fallesen <mfallesen2010@gmail.com>
2020-11-17 18:38:55 -0800

    dynamically render user post components

--- src/components/Userlandingpost/Userlandingpost.js
+++ src/components/Userlandingpost/Userlandingpost.js
@@ -12,20 +12,61 @@ const useStyles = makeStyles({
 export default function Userlandingpost() {
     const classes = useStyles();  
     
+    const userPosts = [
+        {
+            title: "Amazing",
+            image: "https://picsum.photos/150/200",
+            text: "It was an amazing trip"
+        },
+        {
+            title: "Super Fun!",
+            image: "https://picsum.photos/150/200",
+            text: "Local Fun at its bets!"
+        },
+        {
+            title: "Must Go",
+            image: "https://picsum.photos/150/200",
+            text: "small and Local fun"
+        },
+        {
+            title: "great for a weekend",
+            image: "https://picsum.photos/150/200",
+            text: "Great fun and close to home!"
+        },
+        {
+            title: "Amazing",
+            image: "https://picsum.photos/150/200",
+            text: "It was an amazing trip"
+        },
+        {
+            title: "Super Fun!",
+            image: "https://picsum.photos/150/200",
+            text: "Local Fun at its bets!"
+        },
+        {
+            title: "Must Go",
+            image: "https://picsum.photos/150/200",
+            text: "small and Local fun"
+        },
+        {
+            title: "great for a weekend",
+            image: "https://picsum.photos/150/200",
+            text: "Great fun and close to home!"
+        },
+    ]
+
+
     return (
         <div>
             <Grid container>
                 <Grid className={classes.cardRow} container item md={12} spacing={3}>
-                    <UserPostCard/>
-                    <UserPostCard/>
-                    <UserPostCard/>
-                    <UserPostCard/>
-                {/* </Grid>
-                <Grid className={classes.cardRow} container item md={12} spacing={3}> */}
-                    <UserPostCard/>
-                    <UserPostCard/>
-                    <UserPostCard/>
-                    <UserPostCard/>
+                    {userPosts.map((post) =>
+                    <UserPostCard
+                    title={post.title}
+                    image={post.image}
+                    text={post.text}
+                    />
+                    )}
                 </Grid>
 
             </Grid>
