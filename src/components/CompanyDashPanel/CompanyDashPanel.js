e9e0749f6d3f39040cca6ddbbbd0c3c8bc40b94d
Mike Fallesen <mfallesen2010@gmail.com>
2020-11-18 11:06:30 -0800

    add edit  and add buttons for comany interaction

---
+++ src/components/CompanyDashPanel/CompanyDashPanel.js
@@ -0,0 +1,46 @@
+import { Grid } from '@material-ui/core'
+import React from 'react'
+import AddIcon from '@material-ui/icons/Add';
+import { makeStyles } from '@material-ui/core/styles';
+import EditIcon from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(5),
+    },
+}));
+
+export default function CompanyDashPanel() {
+    const classes = useStyles();
+
+
+    return (
+        <Grid 
+            container
+            direction="row"
+            justify="space-evenly"
+            alignItems="center" 
+            xs={12}
+        >
+            <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={classes.button}
+                startIcon={<AddIcon />}
+            >
+                Add Adventure
+            </Button>
+            <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={classes.button}
+                startIcon={<EditIcon />}
+            >
+                Edit Company Profile
+            </Button>
+        </Grid>
+    )
+}
