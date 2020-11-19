07b0af6f3ea4e878f2f59e178616de2ffa96a35c
Mike Fallesen <mfallesen2010@gmail.com>
2020-11-18 22:11:54 -0800

    add button changes when user clicks add adventure button

--- src/components/CompanyDashPanel/CompanyDashPanel.js
+++ src/components/CompanyDashPanel/CompanyDashPanel.js
@@ -1,9 +1,10 @@
-import { Grid } from '@material-ui/core'
-import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
 import AddIcon from '@material-ui/icons/Add';
 import { makeStyles } from '@material-ui/core/styles';
 import EditIcon from '@material-ui/icons/Edit';
 import Button from '@material-ui/core/Button';
+import DashboardIcon from '@material-ui/icons/Dashboard';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -14,26 +15,57 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CompanyDashPanel(props) {
     const classes = useStyles();
+    const [addButton, setAddButton] = useState(false);
 
+    function addButtonChange() {
+        if (addButton) {
+            return setAddButton(false)
+        } else {
+            return setAddButton(true)
+        }
+    }
 
     return (
-        <Grid 
+        <Grid
             container
             direction="row"
             justify="space-evenly"
-            alignItems="center" 
+            alignItems="center"
             xs={12}
         >
-            <Button
-                variant="contained"
-                color="primary"
-                size="large"
-                className={classes.button}
-                startIcon={<AddIcon />}
-                onClick={props.handlePostAdventure}
-            >
-                Add Adventure
-            </Button>
+
+            {addButton ?
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    className={classes.button}
+                    startIcon={<DashboardIcon />}
+                    onClick={() => {
+                        props.handlePostAdventure();
+                        addButtonChange();
+                    }}
+                >
+                    Back to Dashboard
+                </Button> 
+                :
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    className={classes.button}
+                    startIcon={<AddIcon />}
+                    onClick={() => {
+                        props.handlePostAdventure();
+                        addButtonChange();
+                    }}
+                >
+                    Add Adventure
+                </Button>
+            }
+
+
+
             <Button
                 variant="contained"
                 color="primary"
