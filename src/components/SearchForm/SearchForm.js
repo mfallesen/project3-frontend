0d91b48d134f00b6d4f216bffef5060229647cbd
Mike Fallesen <mfallesen2010@gmail.com>
2020-11-20 13:20:15 -0800

    remove styling

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,20 +2,11 @@ import React from 'react';
 import SearchBar from 'material-ui-search-bar';
 import { makeStyles } from '@material-ui/styles'
 
-
-
-const useStyles = makeStyles({
-  searchbarRoot: {
-    marginLeft: 'auto',
-    marginTop: 15,
-  },
-})
-
 export default function SearchForm(props) {
-  const classes = useStyles();
+  
   return (
     <SearchBar
-    className={classes.searchbarRoot}
+    
       value={props.search}
       onChange={props.handleInputChange}
 
