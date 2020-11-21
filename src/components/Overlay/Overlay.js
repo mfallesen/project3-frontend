afca1665a760ed37453791a232c6ccae387cafd2
Larry Cessna <larrybcessna@gmail.com>
2020-11-20 16:47:02 -0800

    Added link to take user to user registration page.

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -4,6 +4,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid"
+import {Link} from 'react-router-dom';
+// import UserSignUp from "../UserSignUp/UserSignUp";
 
 
 
@@ -17,13 +19,16 @@ const useStyles = makeStyles({
 export default function Overlay(props) {
     const classes = useStyles();
 
+    const signup = (props) =>{
+        window.location.href = "/registeruser";
+    }
     return (
         <Grid container className={classes.root}>
             <Grid item xs={12}>
                 <Typography>
                     <h1>Memorable Microadventures</h1>
                 </Typography>
-                <Button className={props.btn}>Sign Up</Button>
+                <Link to={"/registeruser"} ><Button className={props.btn} onclick={signup}>Sign Up</Button></Link>
             </Grid>
         </Grid>
     );
