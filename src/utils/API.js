b2279f4a46be0e956281e458e72766129584bb03
Larry Cessna <larrybcessna@gmail.com>
2020-11-21 21:55:34 -0800

    Removed redundant secure route.

--- src/utils/API.js
+++ src/utils/API.js
@@ -110,20 +110,6 @@ export default {
         })
     },
 
-    getAdventures: function (token) {
-        console.log("In API: ", token)
-
-        return axios({
-            method: 'get',
-            url: BASEURL + '/api/company/adventures/',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-
-        })
-    },
-
     getAdventureLanding: function () {
 
         return axios({
